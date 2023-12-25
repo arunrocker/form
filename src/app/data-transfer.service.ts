c9e62d100dd6db00ef1320872e40317cfec95edc
arunrocker <111231701+arunrocker@users.noreply.github.com>
2023-12-25 18:56:03 +0530

    "service added"

---
+++ src/app/data-transfer.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataTransferService {
+
+  eventDetails:boolean = false;
+  speaker1Details:boolean = false;
+  constructor() { 
+    
+  }
+}
